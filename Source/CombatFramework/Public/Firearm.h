35bbccdbb73359ee35558eeea759c4bfd94718ea
CryfallProductions <official.cryfallproductions@gmail.com>
2026-02-05 18:03:29 -0500

    Update source

---
+++ Source/CombatFramework/Public/Firearm.h
@@ -0,0 +1,19 @@
+// Copyright © 2025, Nathan Adotey. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventory/Items/ItemBase.h"
+#include "Firearm.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATFRAMEWORK_API UFirearm : public UItemBase
+{
+	GENERATED_BODY()
+	
+};
+
+// Copyright © 2025, Nathan Adotey. All Rights Reserved.
